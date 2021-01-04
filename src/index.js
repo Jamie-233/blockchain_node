cafb6cb296fd355bfd81bc67a0299b0ad7ea5a9b
Jenkin <zhaojiancun@yunquna.com>
2021-01-04 23:22:48 +0800

    feat: added cli-table formatted output

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 const vorpal = require('vorpal')()
+const Table = require('cli-table')
 const Blockchain = require('./blockchain')
+
+// instantiate
+// const table = new Table({
+//     head: ['TH 1 label', 'TH 2 label'],
+//     colWidths: [10, 20]
+// });
+
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
+// table.push(
+//     ['First value', 'Second value'],
+//     ['First value', 'Second value']
+// );
+
+function formatLog(data) {
+  console.log(data)
+  if(!Array(data)) {
+    data = [data]
+  }
+  const first = data[0]
+  const head = Object.keys(first)
+  const table = new Table({
+      head,
+      colWidths: new Array(head.length).fill(20)
+  });
+  const res = data.map(v => {
+    return head.map(h=> v[h])
+  })
+
+  table.push(...res);
+  console.log(table.toString());
+}
+
+// table.push(
+//     ['First value', 'Second value'],
+//     ['First value', 'Second value']
+// );
 
 const blockchain = new Blockchain()
 
@@ -8,7 +45,7 @@ vorpal
   .action(function(args, callback) {
     const newBlock = blockchain.mine()
     if(newBlock) {
-      console.log(newBlock)
+      formatLog(newBlock)
     }
     callback()
   })
@@ -16,7 +53,7 @@ vorpal
 vorpal
   .command('chain', '查看区块链')
   .action(function(args, callback) {
-    this.log(blockchain.blockchain)
+    formatLog(blockchain.blockchain)
     callback()
   })
 
